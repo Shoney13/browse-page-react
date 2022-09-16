d2ca444e4fe69b39704ba806cf6fecba606a03d4
Shone Jogi <shonejogi2000@gmail.com>
2022-09-16 16:58:42 +0530

    Added Search Animations

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import styles from './NavBar.module.css'
 import ProfileIcon from './ProfileIcon'
 const NavBar = () => {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
+  const handleSearchClick = () =>{
+    setIsSearchActive(!isSearchActive);
+  }
+
   return (
     <div className={styles.NavBarMain}>
       <div className={styles.NavBarLogo} />
       <div className={styles.NavBarSearchArea}>
-        <button className={styles.NavBarSearchIcon}> 
+        <input type="text" className={`${styles.NavBarSearchInput} ${isSearchActive ? styles.searchActive : ''}`} placeholder='What do you want to learn today?'/>
+        <button className={`${styles.NavBarSearchIcon} ${isSearchActive ? styles.searchActive : ''}`} onClick={handleSearchClick}> 
           <SearchIcon />
         </button>
-        <button className={styles.NavBarCoursesButton}>
+        <button className={`${styles.NavBarCoursesButton} ${isSearchActive ? styles.searchActive : ''}`}>
           Courses
         </button>
-        <span className={styles.NavBarServicesButton}>
+        <span className={`${styles.NavBarServicesButton} ${isSearchActive ? styles.searchActive : ''}`}>
           Our services <KeyboardArrowDownIcon/>
         </span>
         <ProfileIcon/>
