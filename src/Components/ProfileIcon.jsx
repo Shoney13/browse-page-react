c074f45921f303c5d51999f6f63e2a2bce1f3b22
Shone Jogi <shonejogi2000@gmail.com>
2022-09-17 17:38:10 +0530

    Added change type in profile Icon

--- src/Components/ProfileIcon.jsx
+++ src/Components/ProfileIcon.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './ProfileIcon.module.css'
 const ProfileIcon = () => {
+  const [profileType, setProfileType] = useState('S');
+  const handleProfileChange = () => {
+    setProfileType((prevState)=> {
+      if(prevState === 'S') return 'T'
+      else return 'S'
+    })
+  }
   return (
-    <div className={styles.profileIconMain}>
+    <div className={`${styles.profileIconMain} ${profileType === 'S'? styles.student: styles.teacher}`} onClick={handleProfileChange}>
       <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="Profile Icon" className={styles.profileIconImage} />
-      <span className={styles.profileIconType}>
-        S
+      <span className={`${styles.profileIconType} ${profileType === 'S'? styles.student: styles.teacher}`}>
+        {profileType}
       </span>
     </div>
   )
